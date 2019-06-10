58fc1774330d124feff81aea42d466c78163ce64
mick3247652 <mick.dunaev@yandex.ru>
2019-06-10 08:30:35 +0300

    initial commit

---
+++ app/components/Container/styles.js
@@ -0,0 +1,10 @@
+import EStyleSheets from 'react-native-extended-stylesheet'
+
+export default EStyleSheets.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '$primaryBlue',
+    }
+})
---
+++ app/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import Home from "./screens/Home";
+import EStyleSheet from "react-native-extended-stylesheet";
+
+EStyleSheet.build({
+  $primaryBlue: '#4F6D7A',
+})
+
+export default () => <Home />;
