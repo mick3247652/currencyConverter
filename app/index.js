3e76fcf958f87fdf55b47647bcef8f7a64e12c54
mick3247652 <mick.dunaev@yandex.ru>
2019-06-12 10:26:58 +0300

    connect to store

--- app/index.js
+++ app/index.js
@@ -1,20 +1,29 @@
 import React from "react";
 
-import Navigator from './config/routes'
+import Navigator from "./config/routes";
 import EStyleSheet from "react-native-extended-stylesheet";
-import {AlertProvider} from './components/Alert'
+import { AlertProvider } from "./components/Alert";
+
+import { Provider } from "react-redux";
+import store from "./config/store";
 
 EStyleSheet.build({
-  $primaryBlue: '#4F6D7A',
-  $primaryOrange: '#D57A66',
-  $primaryGreen: '#00BD9D',
-  $primaryPurple: '#9E768F',
-  
-  $white: '#FFF',
-  $border: '#E2E2E2',
-  $inputText: '#797979',
-  $lightGray: '#F0F0F0',
-  $darkText: '#343434',
-})
+  $primaryBlue: "#4F6D7A",
+  $primaryOrange: "#D57A66",
+  $primaryGreen: "#00BD9D",
+  $primaryPurple: "#9E768F",
 
-export default () => <AlertProvider><Navigator /></AlertProvider>;
+  $white: "#FFF",
+  $border: "#E2E2E2",
+  $inputText: "#797979",
+  $lightGray: "#F0F0F0",
+  $darkText: "#343434"
+});
+
+export default () => (
+  <Provider store={store}>
+    <AlertProvider>
+      <Navigator />
+    </AlertProvider>
+  </Provider>
+);
